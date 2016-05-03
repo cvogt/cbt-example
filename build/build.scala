0c068697bfcc3ba759027d715e4ca694dd785c6a
Christopher Vogt <oss.nsp@cvogt.org>
2016-05-03 11:20:29 +0300

    add gitignore

--- build/build.scala
+++ build/build.scala
@@ -8,17 +8,7 @@ class Build( context: Context ) extends BasicBuild( context ){
     super.dependencies // don't forget super.dependencies here
     ++
     Resolver( mavenCentral ).bind(
-      // automatically add Scala major version to artifact id
-      // CBT-style Scala dependency 
       ScalaDependency( "com.lihaoyi", "ammonite-ops", "0.5.5" )
-      // or SBT-style Scala dependency
-//      "com.lihaoyi" %% "ammonite-ops" % "0.5.5"
-
-      // don't mess with the artifact id
-      // CBT-Style Java dependency 
-  //    MavenDependency( "com.lihaoyi", "ammonite-ops_2.11", "0.5.5" )
-      // or SBT-style Java dependency
-    //  "com.lihaoyi" % "ammonite-ops_2.11" % "0.5.5"
     )
   )
 }
