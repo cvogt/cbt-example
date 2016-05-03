8f9e7080633df3e44a72ec40695afb5a8b597864
Christopher Vogt <oss.nsp@cvogt.org>
2016-05-03 12:25:37 +0300

    Make main build reproducible

--- build/build.scala
+++ build/build.scala
@@ -5,6 +5,7 @@ import scala.collection.immutable.Seq
 
 import ammonite.ops._
 
+//cbt:https://github.com/cvogt/cbt.git#80da1d862e49b1812c51e1b0c6eb26947d36f26d
 class Build( context: Context ) extends BasicBuild( context ){
   override def dependencies = (
     super.dependencies // don't forget super.dependencies here
